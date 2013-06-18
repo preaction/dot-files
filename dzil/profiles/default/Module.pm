7e0f84d770cf11543eadcddcd45003cea5307941
Doug Bell <doug.bell@baml.com>
2013-06-18 16:50:52 -0400

    rearrange module skeleton and add POD sections for arguments/options

--- dzil/profiles/default/Module.pm
+++ dzil/profiles/default/Module.pm
@@ -1,4 +1,5 @@
 package {{$name}};
+# ABSTRACT: Write a sentence about what it does
 
 use strict;
 use warnings;
@@ -8,14 +9,14 @@ use warnings;
 1;
 __END__
 
-=head1 NAME
-
-{{$name}}
-
 =head1 SYNOPSIS
 
 =head1 DESCRIPTION
 
+=head1 ARGUMENTS
+
+=head1 OPTIONS
+
+=head1 ATTRIBUTES
+
 =head1 METHODS
-
-
