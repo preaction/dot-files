b1ae37d7a67474fb4efec72a95e3df32681cfb1b
Doug Bell <doug@preaction.me>
2018-03-05 15:50:08 -0600

    add didio session

---
+++ tmuxifier-layouts/didio.session.sh
@@ -0,0 +1,21 @@
+# Set a custom session root path. Default is `$HOME`.
+# Must be called before `initialize_session`.
+session_root "~/perl/didio"
+
+# Create session with specified name if it does not already exist. If no
+# argument is given, session name will be based on layout file name.
+if initialize_session "didio"; then
+
+  new_window "code"
+
+  new_window "daemon"
+  tmux split-window -t "$session:$window.0" -v -p 50
+  tmux send-keys -t "$session:$window.0" "cd $session_root; carton exec morbo didio.pl" Enter
+  tmux send-keys -t "$session:$window.1" "cd $session_root; postgres -D db" Enter
+
+  select_window 1
+
+fi
+
+# Finalize session creation and switch/attach to it.
+finalize_and_go_to_session
