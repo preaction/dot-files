92de34c7614d909740f435770084ff7295e414ac
Doug Bell <doug@preaction.me>
2018-03-05 15:51:14 -0600

    add multiple instances of MSD
    This allows for testing moving tickets between dashboards

--- tmuxifier-layouts/managed-services-dashboard.session.sh
+++ tmuxifier-layouts/managed-services-dashboard.session.sh
@@ -11,8 +11,10 @@ if initialize_session "managed-services-dashboard"; then
 
   new_window "test"
   tmux split-window -t "$session:$window.0" -v -p 50
-  tmux send-keys -t "$session:$window.0" "cd $session_root; export PERL5LIB=lib:$PERL5LIB; carton exec morbo script/dashboard" Enter
-  tmux send-keys -t "$session:$window.1" "cd $session_root" Enter
+  tmux split-window -t "$session:$window.0" -h -p 50
+  tmux send-keys -t "$session:$window.0" "cd $session_root; export PERL5LIB=lib:$PERL5LIB; export MOJO_LOG_LEVEL=debug; carton exec morbo script/dashboard -m msd" Enter
+  tmux send-keys -t "$session:$window.1" "cd $session_root; export PERL5LIB=lib:$PERL5LIB; export MOJO_LOG_LEVEL=debug; carton exec morbo script/dashboard -m dcops -l 'http://*:3001'" Enter
+  tmux send-keys -t "$session:$window.2" "cd $session_root; " Enter
 
   new_window "db"
   tmux send-keys -t "$session:$window.0" "cd $session_root; redis-server /usr/local/etc/redis.conf" Enter
