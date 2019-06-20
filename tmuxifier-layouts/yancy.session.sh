5e77090595c4ff5759db9fe2a74ec7c95e1d41a6
Doug Bell <doug@preaction.me>
2019-06-20 08:56:43 -0400

    fix yancy session

--- tmuxifier-layouts/yancy.session.sh
+++ tmuxifier-layouts/yancy.session.sh
@@ -10,15 +10,19 @@ if initialize_session "yancy"; then
 
   new_window "test"
   tmux split-window -t "$session:$window.0" -v -p 50
-  tmux send-keys -t "$session:$window.0" "cd $session_root; export MOJO_HOME=eg; export PERL5LIB=lib:$PERL5LIB; morbo bin/yancy --watch $session_root" Enter
+  tmux send-keys -t "$session:$window.0" "cd $session_root; export MOJO_HOME=eg/test-app; export PERL5LIB=t/lib:lib:$PERL5LIB; morbo eg/test-app/myapp.pl --watch $session_root" Enter
   tmux send-keys -t "$session:$window.1" "cd $session_root; export TEST_ONLINE_MYSQL=mysql://localhost/yancy_mysql_test" Enter
   tmux send-keys -t "$session:$window.1" "cd $session_root; export TEST_ONLINE_PG=postgres://localhost/test" Enter
-  tmux send-keys -t "$session:$window.1" "cd $session_root; export TEST_ONLINE_SQLITE=sqlite:db/sqlite/test.db" Enter
+  tmux send-keys -t "$session:$window.1" "cd $session_root; export TEST_YANCY_EXAMPLES=1" Enter
+  tmux send-keys -t "$session:$window.1" "cd $session_root; export TEST_SELENIUM=1" Enter
+  tmux send-keys -t "$session:$window.1" "cd $session_root; export YANCY_SELENIUM_CAPTURE=1" Enter
 
   new_window "db"
-  tmux split-window -t "$session:$window.0" -v -p 50
+  tmux split-window -t "$session:$window.0" -v -p 30
+  tmux split-window -t "$session:$window.1" -v -p 50
   tmux send-keys -t "$session:$window.0" "cd $session_root; postgres -D db/pg" Enter
-  tmux send-keys -t "$session:$window.1" "cd $session_root; mysqld --skip-grant-tables --datadir=db" Enter
+  tmux send-keys -t "$session:$window.1" "cd $session_root; mysqld --skip-grant-tables --datadir=./db/mysql" Enter
+  tmux send-keys -t "$session:$window.2" "cd $session_root; mongod --dbpath db/mongo" Enter
 
   select_window 1
 
